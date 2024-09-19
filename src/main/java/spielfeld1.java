0777f902df219bc9ae7f9ab2735de14fe17fab59
MayraEge <mayra.ege@btc-ag.com>
2024-09-19 10:55:02 +0200

    RobotWars

--- src/main/java/spielfeld1.java
+++ src/main/java/spielfeld1.java
@@ -1,2 +1,22 @@
 public class spielfeld1 {
-}
+
+    public static void main(String[] args)
+    {
+        int breite=10;
+        int laenge=10;
+        int ausgabe=0;
+        int i=0;
+
+        while(i<laenge)
+        {
+            for(i = 0; i < laenge; i++)
+            {
+                for(int index=0; index<breite; index++)
+                {
+                    ausgabe=ausgabe+1;
+                    System.out.print(ausgabe+" ");
+                }
+                ausgabe=i+1;
+                System.out.println();
+            }}
+    }}
